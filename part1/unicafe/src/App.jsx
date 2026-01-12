298ba3ca04a39f086274757107c70e687c40900d
frarosset <37275581+frarosset@users.noreply.github.com>
2026-01-12 22:13:45 +0100

    Exercise 1.8: unicafe, step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -21,15 +21,30 @@ const FeedbackStat = ({ stat, value }) => (
   </div>
 );
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all; // = (good * 1 + neutral * 0 + bad * -1) / all;
+  const positive = (good / all) * 100;
+
+  return (
+    <div>
+      <Title text="statistics" level={2} />
+      <FeedbackCount feedback="good" count={good} />
+      <FeedbackCount feedback="neutral" count={neutral} />
+      <FeedbackCount feedback="bad" count={bad} />
+
+      <FeedbackStat stat="all" value={all} />
+      <FeedbackStat stat="average" value={average} />
+      <FeedbackStat stat="positive" value={`${positive} %`} />
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const all = good + neutral + bad;
-  const average = (good - bad) / all; // = (good * 1 + neutral * 0 + bad * -1) / all;
-  const positive = (good / all) * 100;
 
   return (
     <div>
@@ -41,16 +56,7 @@ const App = () => {
         <Button text="bad" onClick={giveFeedback(setBad)} />
       </div>
 
-      <div>
-        <Title text="statistics" level={2} />
-        <FeedbackCount feedback="good" count={good} />
-        <FeedbackCount feedback="neutral" count={neutral} />
-        <FeedbackCount feedback="bad" count={bad} />
-
-        <FeedbackStat stat="all" value={all} />
-        <FeedbackStat stat="average" value={average} />
-        <FeedbackStat stat="positive" value={`${positive} %`} />
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };

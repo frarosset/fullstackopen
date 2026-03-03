5b2718d191025c4943de97a8afe97ff8b54962fe
frarosset <37275581+frarosset@users.noreply.github.com>
2026-03-03 16:35:12 +0100

    Exercise 3.3: Phonebook backend step 3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -28,6 +28,18 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+
+  const person = persons.find((p) => p.id == id);
+
+  if (!person) {
+    return res.status(404).end();
+  }
+
+  res.json(person);
+});
+
 app.get("/info", (req, res) => {
   res.send(
     `
